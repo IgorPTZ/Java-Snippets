1db80fe9eee42a81b203ae2f84d66d37f53b1925
Igor <igor.ptz@hotmail.com>
2021-03-10 19:47:38 -0300

    Update

--- src/service/RelatorioService.java
+++ src/service/RelatorioService.java
@@ -28,72 +28,12 @@ public class RelatorioService implements Serializable {
 	
 	private String caminhoArquivoRelatorio = null;
 	
+	@SuppressWarnings({ "deprecation", "rawtypes" })
 	private JRExporter exporter = null;
 	
 	private String caminhoSubReport_Dir = "";
 	
 	private File arquivoGerado = null;
-	
-/*	@SuppressWarnings("unchecked")
-	public String gerarRelatorio0(List<?> listaDeObjetos, HashMap parametrosRelatorio,
-			                     String nomeDoRelatorioJasper, String nomeDeSaida, ServletContext servletContext) throws Exception {
-	
-		
-		// Cria a lista de elementos do relatorio
-		JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(listaDeObjetos);
-		
-		// Fornece o caminho fisico para a pasta que contem os relatorios .jasper
-		String caminhoDoRelatorio = servletContext.getRealPath(FOLDER_RELATORIOS);
-		
-		File arquivo = new File(caminhoDoRelatorio + SEPARADOR + nomeDoRelatorioJasper + ".jasper");
-		
-		if(caminhoDoRelatorio == null
-		  || (caminhoDoRelatorio != null && caminhoDoRelatorio.isEmpty())
-		  || !arquivo.exists()) {
-			
-			caminhoDoRelatorio = this.getClass().getResource(FOLDER_RELATORIOS).getPath();
-			
-			SEPARADOR = "";
-		}
-		
-		// Caminho completo para o relatorio compilado 
-		String caminhoParaArquivosJasper = caminhoDoRelatorio + SEPARADOR + nomeDoRelatorioJasper + ".jasper";
-		
-		
-		// Realiza o carregamento do relatorio 
-		JasperReport relatorioJasper = (JasperReport) JRLoader.loadObjectFromFile(caminhoParaArquivosJasper);
-		
-		
-		// Preenche o parametro SUBREPORT_DIR com o caminho fisico
-		caminhoDoSubReport_Dir = caminhoDoRelatorio + SEPARADOR;
-		
-		parametrosRelatorio.put(SUBREPORT_DIR, caminhoDoSubReport_Dir);
-		
-		// Carrega o arquivo para a memoria
-		JasperPrint jasperPrint = JasperFillManager.fillReport(relatorioJasper, parametrosRelatorio, jrBeanCollectionDataSource);
-			
-		
-		// Caminho do relatorio exportado
-		exporter = new JRPdfExporter();
-		
-		caminhoArquivoRelatorio = caminhoDoRelatorio + SEPARADOR + nomeDeSaida + ".pdf";
-		
-		// Cria novo arquivo exportado
-		arquivoGerado = new File(caminhoArquivoRelatorio);
-		
-		// Prepara a impressao 
-		exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
-		
-		exporter.setParameter(JRExporterParameter.OUTPUT_FILE, arquivoGerado);
-		
-		// BExecuta a exportacao 
-		exporter.exportReport();
-		
-		// Remove o arquivo do servidor apos ser feito o download
-		arquivoGerado.deleteOnExit();
-		
-		return caminhoArquivoRelatorio;
-	}   */
 	
 	@SuppressWarnings({ "unchecked", "deprecation", "rawtypes" })
 	public String gerarRelatorio(List<?> listaDataBeanColletion, HashMap parametrosRelatorio
